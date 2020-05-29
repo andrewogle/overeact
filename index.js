7627318370cdb8b2b5c3f528a3a49f441043a9b5
Andrew <andrewryanogle@gmail.com>
2020-05-29 07:43:31 -0700

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+function createElement(type, props, ...children){
+    return {
+        type,
+        props:{
+            ...props,
+            children: children.map(child =>
+                typeof child === "object"
+                ? child
+                : createTextElement(child)
+                ),
+        },
+    }
+}
